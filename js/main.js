1635d956d57a265b6db3601cf2846c09121b344f
Andrew Tibbs <andrew@switchyardcreative.com>
2013-09-09 23:35:26 -0700

    desktop styles and functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+$(".upcoming-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#upcoming").offset().top
+    }, 500);
+});
+$(".about-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 500);
+});
+$(".past-speakers-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#past-speakers").offset().top
+    }, 500);
+});
+
+$(".top-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("body").offset().top
+    }, 500);
+});
