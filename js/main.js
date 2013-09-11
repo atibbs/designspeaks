353cb801d9a5e045cad2530e6cbd5216ca3c6e52
Andrew Tibbs <andrew@switchyardcreative.com>
2013-09-11 10:33:07 -0700

    adds js libraries and responsive fixes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
+$(document).ready(function() {
 
-$(".upcoming-btn").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#upcoming").offset().top
-    }, 500);
-});
-$(".about-btn").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#about").offset().top
-    }, 500);
-});
-$(".past-speakers-btn").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#past-speakers").offset().top
-    }, 500);
-});
+	// Sticky Navigation
+	$("#nav").sticky({topSpacing:0});
 
-$(".top-btn").click(function() {
-    $('html, body').animate({
-        scrollTop: $("body").offset().top
-    }, 500);
+
+	$(".upcoming-btn").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#upcoming").offset().top
+	    }, 500);
+	});
+	$(".about-btn").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#about").offset().top
+	    }, 500);
+	});
+	$(".past-speakers-btn").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#past-speakers").offset().top
+	    }, 500);
+	});
+
+	$(".top-btn").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("body").offset().top
+	    }, 500);
+	});
+
+
+	// Fittext headline
+	jQuery("h1.title").fitText(1.2, { minFontSize: '40px', maxFontSize: '105px' });
+
+
 });
